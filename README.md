0c3c7fa7ff52fc6aebd47a63ef9c04197a071c14
Vadym Shapoval <vadym.shapoval@alertinnovation.com>
2024-09-06 14:09:59 +0300

    first commit

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 > #### Run Server and send message from client to the server
-> - run [Hl7StartServer](src/main/java/org/example/hl7/Main.java) main method, 
+> - run [Main class's](src/main/java/org/example/hl7/Main.java) main method, 
 > - message will be sent and an response acknowledge message received. 
 
 
--- src/main/java/org/example/hl7/client/Hl7Client.java
+++ src/main/java/org/example/hl7/client/Hl7Client.java
@@ -1,6 +1,7 @@
 package org.example.hl7.client;
 
 import ca.uhn.hl7v2.DefaultHapiContext;
+import ca.uhn.hl7v2.HL7Exception;
 import ca.uhn.hl7v2.HapiContext;
 import ca.uhn.hl7v2.app.Connection;
 import ca.uhn.hl7v2.app.Initiator;
@@ -10,21 +11,28 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public class Hl7Client {
-    private String serverHost;
-    private int port;
-    private boolean useTls;
+    private HapiContext context;
+    /**
+     * A connection object represents a socket attached to an HL7 server
+     */
+    private Connection connection;
+
+    public Hl7Client(String serverHost, int port, boolean useTls) {
+        try {
+            this.context = new DefaultHapiContext();
+            this.connection = context.newClient(serverHost, port, useTls);
+        } catch (HL7Exception ex) {
+            throw new IllegalStateException(ex);
+        }
+    }
 
     public String sendMessage(final String stringMessage) throws Exception {
-        HapiContext context = new DefaultHapiContext();
         Parser pipeParser = context.getPipeParser();
         Message message = pipeParser.parse(stringMessage);
-        // A connection object represents a socket attached to an HL7 server
-        Connection connection = context.newClient(serverHost, port, useTls);
-        // creates a client, which will connect to our waiting server and send messages to it.
+        // connection creates a client, which will connect to waiting server and send messages to it.
         // The initiator is used to transmit unsolicited messages
         Initiator initiator = connection.getInitiator();
         Message response = initiator.sendAndReceive(message);
-        String responseString = pipeParser.encode(response);
 
         /*
          * MSH|^~\&|||||20070218200627.515-0500||ACK|54|P|2.2 MSA|AA|12345
@@ -39,14 +47,17 @@ public class Hl7Client {
          *         response = initiator.sendAndReceive(message);
          */
 
-        /*
-         * Close the connection when you are done with it. If you are designing a system which will continuously send out messages, you may want to
-         * consider not closing the connection until you have no more messages to send out. This is more efficient, as most (if not all) HL7 receiving
-         * applications are capable of receiving lots of messages in a row over the same connection, even with a long delay between messages.
-         *
-         * See http://hl7api.sourceforge.net/xref/ca/uhn/hl7v2/examples/SendLotsOfMessages.html for an example of this.
-         */
+        return pipeParser.encode(response);
+    }
+
+    /*
+     * Close the connection when you are done with it. If you are designing a system which will continuously send out messages, you may want to
+     * consider not closing the connection until you have no more messages to send out. This is more efficient, as most (if not all) HL7 receiving
+     * applications are capable of receiving lots of messages in a row over the same connection, even with a long delay between messages.
+     *
+     * See http://hl7api.sourceforge.net/xref/ca/uhn/hl7v2/examples/SendLotsOfMessages.html for an example of this.
+     */
+    public void closeConnection() {
         connection.close();
-        return responseString;
     }
 }
